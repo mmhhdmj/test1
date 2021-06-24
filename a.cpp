0603c67c5eb462ccd7453a428fc487cbcf4698be
Yubin <mmhhdmj@gmail.com>
2021-06-24 21:19:30 +1000

    change 2 to 3 in line 15
    testing

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printbinary(int val)
+{
+	for(int i=10;i>=0;i--)
+	if(val & (1<<i)) cout<<"1";
+		else cout<<"0";
+		cout<<endl;
+}
+
+int main()
+{
+
+printbinary(2);
+
+	return 0;
+}
